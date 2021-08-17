e31f0c71eac36b9a38d1974905b07680de9fa2cb
Pedro Henrique <phcatanduba@gmail.com>
2021-08-17 11:13:40 -0300

    fonts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from './App';
-
+import './assets/styles/fonts.css';
 import './assets/styles/reset.css';
+
+import App from './App';
 
 render(<App />, document.querySelector('.root'));
